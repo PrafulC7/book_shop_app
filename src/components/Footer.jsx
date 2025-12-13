c3146ddd64cc2df920370acd092e67b43f3a9327
PrafulC7 <prafullacharde00715@gmail.com>
2025-12-13 06:21:32 +0530

    chore: class to className

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,14 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-   <footer class="bg-dark text-white p-4 text-center">
-    <div class="container">
+   <footer className="bg-dark text-white p-4 text-center">
+    <div className="container">
         <p class="mb-0">&copy; 2025 Book Store. All rights reserved.</p>
-        {/* <div>
-            <a href="#" class="text-white mx-2">Home</a> |
-            <a href="#" class="text-white mx-2">Features</a> |
-            <a href="#" class="text-white mx-2">About</a>
-        </div> */}
     </div>
 </footer>
   )
